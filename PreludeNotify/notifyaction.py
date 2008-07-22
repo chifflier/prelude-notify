abd1509daa73eb750d4c5688f0a31d51b32bbe56
Yoann Vandoorselaere <yoann.v@prelude-ids.com>
2008-07-22 16:40:55 +0000

    Set priority.

--- PreludeNotify/notifyaction.py
+++ PreludeNotify/notifyaction.py
@@ -4,17 +4,22 @@ class PreludeNotify:
         def __init__(self):
                 pynotify.init("PreludeNotify")
 
-        def _view_cb(self, notification, action, data=None):
+        def _view_cb(self, n, action):
+            print action
             print "view_cb"
             notification.close()
 
-        def _prewikka_view_cb(self, notification, action, data=None):
+        def _prewikka_view_cb(self, n, action):
             print "prewikka_view_cb"
             notification.close()
 
-        def run(self, imageuri, title, message):
+        def run(self, imageuri, urgency, title, message):
                 n = pynotify.Notification(title, message, imageuri)
-                n.add_action("clicked", "View", self._view_cb, None)
-                n.add_action("clicked", "Prewikka View", self._prewikka_view_cb, None)
+
+                if urgency is not None:
+                    n.set_urgency(urgency)
+
+                n.add_action("view", "View", self._view_cb)
+                n.add_action("pview", "Prewikka View", self._prewikka_view_cb)
                 n.show()
 
