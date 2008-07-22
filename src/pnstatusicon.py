e07af44cf144e2fcad13f419dc3fa498fb51054f
Sebastien Tricaud <s.tricaud@inl.fr>
2008-07-22 09:42:29 +0000

    menu with callbacks

--- src/pnstatusicon.py
+++ src/pnstatusicon.py
@@ -8,9 +8,29 @@ class PreludeStatusIcon():
 
 	def menu(self, icon, event_button, event_time):
 		m = gtk.Menu()
+
 		item = gtk.MenuItem('Configure...')
+		item.connect('activate', self.configure)
 		item.show()
 		m.append(item)
+		item = gtk.MenuItem('About')
+		item.connect('activate', self.about)
+		item.show()
+		m.append(item)
+		item = gtk.MenuItem('Quit')
+		item.connect('activate', self.quit)
+		item.show()
+		m.append(item)
+
 		m.popup(None, None, gtk.status_icon_position_menu, event_button,
 			event_time, icon)
+
+	def configure(self, data):
+		print "Run the configure window"
+
+	def about(self, data):
+		print "Run the about window"
+
+	def quit(self, data):
+		gtk.main_quit()
 
