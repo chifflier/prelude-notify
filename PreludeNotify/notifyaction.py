f2e9ef87a3e446574af494ef589b445ea6b3880d
Yoann Vandoorselaere <yoann.v@prelude-ids.com>
2008-07-22 15:11:07 +0000

    Add default action.

--- PreludeNotify/notifyaction.py
+++ PreludeNotify/notifyaction.py
@@ -4,7 +4,17 @@ class PreludeNotify:
         def __init__(self):
                 pynotify.init("PreludeNotify")
 
+        def _view_cb(self, notification, action, data=None):
+            print "view_cb"
+            notification.close()
+
+        def _prewikka_view_cb(self, notification, action, data=None):
+            print "prewikka_view_cb"
+            notification.close()
+
         def run(self, imageuri, title, message):
                 n = pynotify.Notification(title, message, imageuri)
+                n.add_action("clicked", "View", self._view_cb, None)
+                n.add_action("clicked", "Prewikka View", self._prewikka_view_cb, None)
                 n.show()
 
