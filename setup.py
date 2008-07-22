f06e8d93263d097da59e73270dde4910737c3628
Yoann Vandoorselaere <yoann.v@prelude-ids.com>
2008-07-22 14:41:39 +0000

    Update, generate siteconfig.py

--- setup.py
+++ setup.py
@@ -1,13 +1,24 @@
 #!/usr/bin/env python
 
-import sys
-import os, os.path
-import stat
-import glob
-
+import os, glob
 from distutils.core import setup
+from distutils.command.install import install
 
 _VERSION="0.0.0"
+
+class my_install(install):
+   def init_siteconfig(self):
+        config = open("PreludeNotify/siteconfig.py", "w")
+        print >> config, "prefix = '%s'" % os.path.abspath(self.prefix)
+        print >> config, "version = '%s'" % _VERSION
+        config.close()
+
+   def run(self):
+        os.umask(022)
+        self.init_siteconfig()
+        install.run(self)
+
+
 
 setup(name="prelude-notify",
       version=_VERSION,
@@ -16,4 +27,5 @@ setup(name="prelude-notify",
       url = "http://www.prelude-ids.com",
       packages=[ 'PreludeNotify' ],
       data_files=[ ("share/prelude-notify/themes/default", glob.glob("pixmaps/themes/default/*")) ],
-      scripts=[ "prelude-notify" ])
+      scripts=[ "prelude-notify" ],
+      cmdclass={ 'install': my_install } )
