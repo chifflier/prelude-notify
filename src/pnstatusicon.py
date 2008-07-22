32690425b361197d53383d1a7a025ee991917804
Sebastien Tricaud <s.tricaud@inl.fr>
2008-07-22 09:33:19 +0000

    menu for tray icon

--- src/pnstatusicon.py
+++ src/pnstatusicon.py
@@ -4,5 +4,13 @@ import pnconfig
 class PreludeStatusIcon():
 	def __init__(self):
 		icon = gtk.status_icon_new_from_file(pnconfig.iconok)
+		icon.connect('popup-menu', self.menu)
 
+	def menu(self, icon, event_button, event_time):
+		m = gtk.Menu()
+		item = gtk.MenuItem('Configure...')
+		item.show()
+		m.append(item)
+		m.popup(None, None, gtk.status_icon_position_menu, event_button,
+			event_time, icon)
 
