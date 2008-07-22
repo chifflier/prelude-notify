3b6bca12a8bc0ca11fa5f3aa3ee555b0cc658ff3
Sebastien Tricaud <s.tricaud@inl.fr>
2008-07-22 08:48:47 +0000

    cleanup PollIDMEF and added timeout to RecvIDMEF

--- src/prelude-notify.py
+++ src/prelude-notify.py
@@ -17,9 +17,9 @@ pool.AddConnection(Connection("127.0.0.1"))
 
 c.Start()
 
-def PollIDMEF(self):
+def PollIDMEF():
 	idmef = PreludeEasy.IDMEF()
-	ret = c.RecvIDMEF(idmef)
+	ret = c.RecvIDMEF(idmef, 100)
 	if ret:
 		Notify.run(idmef.Get("alert.source(0).node.address(0).address") or "(unknown)",
 				idmef.Get("alert.classification.text"))
@@ -27,6 +27,6 @@ def PollIDMEF(self):
 
 #print pnconfig.iconok
 icon = gtk.status_icon_new_from_file(pnconfig.iconok)
-gobject.idle_add(PollIDMEF, 1)
+gobject.idle_add(PollIDMEF)
 gtk.main()
 
