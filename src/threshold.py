156f54a0d1f765b1630e7b75732653340cef893d
Yoann Vandoorselaere <yoann.v@prelude-ids.com>
2008-07-22 13:17:11 +0000

    Thresholding support.

---
+++ src/threshold.py
@@ -0,0 +1,39 @@
+import gobject
+
+class ThresholdItem:
+    pass
+
+class Threshold:
+    def __init__(self, expire_cb):
+        self._limited = { }
+        self._timeout = 5
+        self._expire_cb = expire_cb
+        self._limit_path = [ "alert.source(0).node.address(0).address", "alert.classification.text" ]
+
+    def _timer_cb(self, item):
+        self._expire_cb(item)
+        return False
+
+    def _newEntry(self, key, idmef):
+        item = self._limited[key] = ThresholdItem()
+        item.count = 1
+        item.idmef = idmef
+        item.timer = gobject.timeout_add(self._timeout * 1000, self._timer_cb, item)
+
+    def _updateEntry(self, key):
+        item = self._limited[key]
+        item.count = item.count + 1
+        gobject.source_remove(item.timer)
+        item.timer = gobject.timeout_add(self._timeout * 1000, self._timer_cb, item)
+
+    def thresholdMessage(self, idmef):
+        rl = []
+
+        for i in self._limit_path:
+            rl += [idmef.Get(i)]
+
+        key = str(rl)
+        if self._limited.has_key(key):
+            self._updateEntry(key)
+        else:
+            self._newEntry(key, idmef)
