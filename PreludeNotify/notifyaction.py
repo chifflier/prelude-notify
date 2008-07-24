bb1cfa5e1b5ff39fea4a2a3ae7676eae8afde65a
Sebastien Tricaud <s.tricaud@inl.fr>
2008-07-24 10:55:03 +0000

    Removed hardcoded values; Handle alert in prewikka

--- PreludeNotify/notifyaction.py
+++ PreludeNotify/notifyaction.py
@@ -1,20 +1,18 @@
-import pygtk
-import gtk
 import pynotify
 
 class PreludeNotify:
         def __init__(self):
                 pynotify.init("PreludeNotify")
 
-        def _view_cb(self, n, action):
-            print "view_cb"
-            notification.close()
-            gtk.main_quit()
-
-        def _prewikka_view_cb(self, n, action):
-            print "prewikka_view_cb"
-            notification.close()
-            gtk.main_quit()
+#        def _view_cb(self, n, action):
+#            print "view_cb"
+#            notification.close()
+#            gtk.main_quit()
+#
+#        def _prewikka_view_cb(self, n, action):
+#            print "prewikka_view_cb"
+#            notification.close()
+#            gtk.main_quit()
 
         def run(self, imageuri, urgency, title, message):
                 n = pynotify.Notification(title, message, imageuri)
@@ -22,9 +20,7 @@ class PreludeNotify:
                 if urgency is not None:
                     n.set_urgency(urgency)
 
-                n.add_action("view", "View", self._view_cb)
-                n.add_action("pview", "Prewikka View", self._prewikka_view_cb)
+#                n.add_action("view", "View", self._view_cb)
+#                n.add_action("pview", "Prewikka View", self._prewikka_view_cb)
                 n.show()
-
-                gtk.main()
 
