0b87145d80c361d0473ee00c58320e8247638e45
Yoann Vandoorselaere <yoann.v@prelude-ids.com>
2008-08-04 11:03:32 +0000

    Make ErrorDialog viable for generic error.

--- PreludeNotify/ErrorDialog.py
+++ PreludeNotify/ErrorDialog.py
@@ -1,13 +1,16 @@
 import os
 import gtk
 
-class CreateProfile:
-	def __init__(self, client, perms, profilename):
-		dlgstring = "Please register your Prelude agent. Run as root:\nprelude-admin register \"%s\" \"idmef:r\" 127.0.0.1 --uid %d --gid %d" % (profilename, os.getuid(), os.getgid())
-                dialog = gtk.MessageDialog(None, 0, gtk.MESSAGE_ERROR, gtk.BUTTONS_CLOSE, dlgstring)
+class ErrorDialog:
+        def __init__(self, content):
+                dialog = gtk.MessageDialog(None, 0, gtk.MESSAGE_ERROR, gtk.BUTTONS_CLOSE, content)
 
                 dialog.show_all()
                 response = dialog.run()
 
                 dialog.destroy()
 
+class CreateProfile(ErrorDialog):
+        def __init__(self, profilename, manager_addr):
+                dlgstring = "Please register your Prelude agent. Run as root:\nprelude-admin register \"%s\" \"idmef:r\" %s --uid %d --gid %d" % (profilename, manager_addr, os.getuid(), os.getgid())
+                ErrorDialog.__init__(self, dlgstring)
