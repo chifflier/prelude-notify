75abe4d97cef10d9116ff01b538db9c4e4417bf4
Yoann Vandoorselaere <yoann.v@prelude-ids.com>
2008-08-04 10:46:13 +0000

    Whitespace police.

--- PreludeNotify/ManagerConnection.py
+++ PreludeNotify/ManagerConnection.py
@@ -5,54 +5,54 @@ import gobject
 import os
 
 class Session:
-	_addrevents = {}
+        _addrevents = {}
 
-	def __init__(self, env, hbmonitor):
-		self.con = None
-		self.env = env
-		self.hbmonitor = hbmonitor
+        def __init__(self, env, hbmonitor):
+                self.con = None
+                self.env = env
+                self.hbmonitor = hbmonitor
 
-	def doConnect(self):
-        	#try:
-	        self.con.Connect(self.env.client, ClientEasy.IDMEF_READ)
-	        #except:
-	        #       ProfileRegister.CreateProfile(c, ClientEasy.IDMEF_READ, env.config.get("idmef", "profile"))
-	        #       sys.exit(1)
-        	self.env.notify.run(None, None, "Manager Connection successfull", "With Prelude-Manager <b>%s</b>" % self.con.GetPeerAddr())
+        def doConnect(self):
+                #try:
+                self.con.Connect(self.env.client, ClientEasy.IDMEF_READ)
+                #except:
+                #       ProfileRegister.CreateProfile(c, ClientEasy.IDMEF_READ, env.config.get("idmef", "profile"))
+                #       sys.exit(1)
+                self.env.notify.run(None, None, "Manager Connection successfull", "With Prelude-Manager <b>%s</b>" % self.con.GetPeerAddr())
 
-	def handleDisconnect(self, err=None):
-		self.env.notify.run("high", None, "Manager Connection interrupted", "With Prelude-Manager <b>%s</b>" % self.con.GetPeerAddr())
-		gobject.timeout_add(10000, self.doConnect)
+        def handleDisconnect(self, err=None):
+                self.env.notify.run("high", None, "Manager Connection interrupted", "With Prelude-Manager <b>%s</b>" % self.con.GetPeerAddr())
+                gobject.timeout_add(10000, self.doConnect)
 
-	def ConnectAddresses(self, manager_addresses):
-		manager_addresses = manager_addresses.split(',')
-		for addr in manager_addresses:
-		        self.con = Connection(addr)
-		        self.doConnect()
-		        self._addrevents[addr] = gobject.io_add_watch(self.con.GetFd(), gobject.IO_IN|gobject.IO_PRI|gobject.IO_HUP|gobject.IO_NVAL|gobject.IO_ERR, self.io_cb, self.con)
+        def ConnectAddresses(self, manager_addresses):
+                manager_addresses = manager_addresses.split(',')
+                for addr in manager_addresses:
+                        self.con = Connection(addr)
+                        self.doConnect()
+                        self._addrevents[addr] = gobject.io_add_watch(self.con.GetFd(), gobject.IO_IN|gobject.IO_PRI|gobject.IO_HUP|gobject.IO_NVAL|gobject.IO_ERR, self.io_cb, self.con)
 
-	def delCon(self):
-		for addr in self._addrevents:
-			gobject.source_remove(self._addrevents[addr])
+        def delCon(self):
+                for addr in self._addrevents:
+                        gobject.source_remove(self._addrevents[addr])
 
-	def io_cb(self, fd, cond, con):
-		if cond & gobject.IO_IN:
-			try:
-				idmef = con.RecvIDMEF()
-			except PreludeEasy.PreludeError, err:
-				if err.GetErrorCode() == -8388614:
-					self.handleDisconnect(con)
-					return False
+        def io_cb(self, fd, cond, con):
+                if cond & gobject.IO_IN:
+                        try:
+                                idmef = con.RecvIDMEF()
+                        except PreludeEasy.PreludeError, err:
+                                if err.GetErrorCode() == -8388614:
+                                        self.handleDisconnect(con)
+                                        return False
 
-			if idmef.Get("heartbeat.create_time"):
-				self.hbmonitor.heartbeat(idmef)
+                        if idmef.Get("heartbeat.create_time"):
+                                self.hbmonitor.heartbeat(idmef)
 
-			elif (not self.env.criteria) or (self.env.criteria.match(idmef)):
-				self.env.thresholding.thresholdMessage(idmef)
+                        elif (not self.env.criteria) or (self.env.criteria.match(idmef)):
+                                self.env.thresholding.thresholdMessage(idmef)
 
-		if cond & gobject.IO_ERR or cond & gobject.IO_HUP:
-			self.handleDisconnect(con)
-			return False
+                if cond & gobject.IO_ERR or cond & gobject.IO_HUP:
+                        self.handleDisconnect(con)
+                        return False
 
-		return True
+                return True
 
